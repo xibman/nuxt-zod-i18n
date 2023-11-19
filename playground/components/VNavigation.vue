c50b0087b1bb71aa4649ae5821265cbca7e74e92
Julien Georget <julien.georget@hikoala.com>
2023-11-19 12:15:52 +0100

    style(linting): linting

--- playground/components/VNavigation.vue
+++ playground/components/VNavigation.vue
@@ -20,7 +20,8 @@ const items = [
 <template>
   <ul class="flex gap-6">
     <li v-for="{ name, label } in items" :key="name">
-      <ULink :to="localeRoute({ name })" active-class="text-primary"
+      <ULink
+:to="localeRoute({ name })" active-class="text-primary"
         inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
         {{ label }}
       </ULink>

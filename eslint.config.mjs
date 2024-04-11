360da10ce495189138702c61ad78190216e49248
Julien Georget <julien.georget@hikoala.com>
2024-04-11 09:53:34 +0200

    style(linting): use eslint v9 and flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+// @ts-check
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+import eslintPluginJsonc from 'eslint-plugin-jsonc'
+
+// Run `npx @eslint/config-inspector` to inspect the resolved config interactively
+export default createConfigForNuxt({
+  features: {
+    // Rules for module authors
+    tooling: true,
+    // Rules for formatting
+    stylistic: true,
+    typescript: true,
+  },
+  dirs: {
+    src: ['./playground', './docs'],
+  },
+}).append([
+  ...eslintPluginJsonc.configs['flat/recommended-with-jsonc'],
+  {
+    rules: {
+      'jsonc/sort-keys': [
+        'error',
+        'asc',
+        {
+          caseSensitive: true,
+          natural: false,
+          minKeys: 2,
+          allowLineSeparatedGroups: false,
+        },
+      ],
+    },
+  },
+  {
+    files: ['package.json'],
+    rules: {
+      'jsonc/sort-keys': 'off',
+    },
+  },
+])

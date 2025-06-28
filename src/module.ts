8fc822fb2e78c9c6ae40657201442c7c4c306a4b
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-06-28 18:17:05 +0200

    chore(module): drop nuxt version check
    Nuxt disables incompatible modules by default.

--- src/module.ts
+++ src/module.ts
@@ -4,7 +4,6 @@ import {
   addPlugin,
   createResolver,
   defineNuxtModule,
-  hasNuxtCompatibility,
   useLogger,
 } from '@nuxt/kit'
 import type { NuxtI18nOptions, LocaleObject } from '@nuxtjs/i18n'
@@ -74,10 +73,6 @@ export default defineNuxtModule<ModuleOptions>().with({
 
     if (checkI18nAvailable) {
       logger.fatal('Nuxt I18n required')
-    }
-
-    if (!hasNuxtCompatibility({ nuxt: '>=3.0.0' }, nuxt)) {
-      logger.error('Nuxt 3 or later required')
     }
 
     if (options.useModuleLocale) {
